7f399d0242bfc17ee00308da43afc0229c86c48e
Jordan J. Mugenyi <jordanjamesxvi@outlook.com>
2022-03-21 04:47:02 +0000

    Add files via upload
    First part to LAB 2-1.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lab2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> cw = Console.WriteLine;
+            int[] numbers = new int[20];
+            int currentIndex = 0;
+            
+
+
+
+            bool repeat = true;
+            do
+            {
+                cw("Please enter an interger: ");
+                string line = Console.ReadLine();
+                if (line == "exit")
+                {
+                    repeat = false;
+                }
+                else
+                {
+
+                    try
+                    {
+                        int number = int.Parse(line);
+                        cw("The number you entered was: " + number);
+                        numbers[currentIndex] = number;
+                        currentIndex++;
+
+                    }
+                    catch (FormatException)
+                    {
+                        cw("That wasn't a number!");
+                    }
+                }
+
+            } while (repeat);
+            Console.WriteLine("Press any key to exit.");
+            {
+                while (repeat) ;
+
+                for (int i = 0; i < currentIndex; i++)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+
+            }
+
+        }
+    }
+}
